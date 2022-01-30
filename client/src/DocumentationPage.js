cbecca5f1837c4916143283f1860dffdd392e95c
BarWieleba <barwieleba@gmail.com>
2022-01-30 20:35:45 +0100

    zmiana w dokumentacji

--- client/src/DocumentationPage.js
+++ client/src/DocumentationPage.js
@@ -12,8 +12,21 @@ export default () => {
             <br></br>
             <Link to="/fibcalc">Fibonacci Calculator</Link>
             <br></br>
-            Im some documentation page!
-            <Link to="/">Go back home</Link>
+
+            <ol>
+                <h3>
+                    <p>Uruchomienie programu:</p>
+                    <li>Klonowanie repozytorium: git clone https://github.com/BarWieleba/PFSwChO.git</li>
+                    <li>W client wykonać npm install </li>
+                    <li>W client wykonać  npm start </li>
+                    <li>W client wykonać docker-compose build</li>
+                    <li>W client wykonać docker-compose up</li>
+                </h3>
+            </ol>
+            <p>
+                W programie z zadania laboratorium 9 zmieniono układ stron i zmieniono menu, pojawiły się w nim nowe elementy.
+                Ponad to dodano menu do każdej strony w celu prostszej nawigacji.
+            </p>
         </div>
     );
 };
